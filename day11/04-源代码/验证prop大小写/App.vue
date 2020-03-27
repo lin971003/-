2a323b3fb3024d3f02768244713f25e055c0c1ea
joven wang <494053175@qq.com>
2020-03-27 21:56:15 +0800

    ok

---
+++ day11/04-源代码/验证prop大小写/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>我是子组件</h2>
+    <Child msg="不要早恋" parentMsg="还是吃得太饱"/>
+  </div>
+</template>
+
+<script>
+import Child from './Child'
+export default {
+  components:{
+    Child
+  }
+}
+</script>
+
+<style></style>
---
+++ day11/04-源代码/验证prop大小写/Child.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="child">
+    <h2>我是子组件</h2>
+    <h3>父对子说:{{msg}}</h3>
+    <h3>父对子说:{{parentMsg}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  props:['msg','parentMsg']
+}
+</script>
+
+<style>
+.child{
+  border:1px solid green;
+}
+</style>
